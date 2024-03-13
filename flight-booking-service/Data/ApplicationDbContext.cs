c76cfb804150e7fc3695d2c571b06a1a63395a23
firuz-macbook <razackoff@gmail.com>
2024-03-13 21:18:46 +0200

    Add database

---
+++ flight-booking-service/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using flight_booking_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hotel_booking_service.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Airline> Airlines { get; set; }
+    public DbSet<Destination> Destinations { get; set; }
+    public DbSet<Flight> Flights { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
---
+++ flight-booking-service/Models/Coordinates.cs
@@ -0,0 +1,7 @@
+namespace flight_booking_service.Models;
+
+public class Coordinates
+{
+    public double Latitude { get; set; } // Широта
+    public double Longitude { get; set; } // Долгота
+}
