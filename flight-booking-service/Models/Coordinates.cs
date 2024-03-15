ba9f69ff8cff3be0dcf52607cb30e5227b37ad59
firuz-macbook <razackoff@gmail.com>
2024-03-15 06:23:33 +0200

    Add ID to Coordinates.

--- flight-booking-service/Models/Coordinates.cs
+++ flight-booking-service/Models/Coordinates.cs
@@ -2,6 +2,7 @@ namespace flight_booking_service.Models;
 
 public class Coordinates
 {
+    public string Id { get; set; }
     public double Latitude { get; set; } // Широта
     public double Longitude { get; set; } // Долгота
 }
