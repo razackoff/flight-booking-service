5d194e868c00d8e2ef2110ce8d6c9a47f4e14050
firuz-macbook <razackoff@gmail.com>
2024-03-17 07:17:20 +0200

    Change Coordinates to CoordinatesID

--- flight-booking-service/Models/Destination.cs
+++ flight-booking-service/Models/Destination.cs
@@ -5,7 +5,7 @@ public class Destination
     public string Id { get; set; } // Идентификатор места назначения
     public string Name { get; set; } // Название места назначения
     public string Code { get; set; } // Код места назначения (например, IATA код)
-    public Coordinates Coordinates { get; set; } // Географические координаты
+    public string CoordinatesID { get; set; } // Географические координаты
     public string AdditionalInfo { get; set; } // Дополнительные данные о месте назначения
 
     // Конструктор для создания нового места назначения
